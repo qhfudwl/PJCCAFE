506319c6c3fcaa018a2c5992482f7d2ba343c4fd
Owner <Owner@PC-2004EOV>
2021-12-05 14:24:16 +0900

    hi first commit

---
+++ JVX330-PJ-C/src/main/java/cafe/pj/jvx330/HomeController.java
@@ -0,0 +1,21 @@
+package cafe.pj.jvx330;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+	
+	public static void main(String[] args) {
+		System.out.println("hi PJC");
+	}
+}
